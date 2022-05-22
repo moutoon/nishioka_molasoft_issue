282b8473243ade8ab92f5d3e745c479008ae9431
moutoon <moutoonm342@gmail.com>
2022-05-22 21:06:13 +0900

    Step1 実装

---
+++ database/migrations/2022_05_22_072759_create_bands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('名前');
+            $table->string('genre')->comment('ジャンル');
+            $table->string('num_people')->comment('人数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bands');
+    }
+};
---
+++ database/migrations/2022_05_22_072904_create_staffs_bands_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs_bands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('staff_id')->comment('部員ID');
+            $table->foreignId('band_id')->comment('バンドID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs_bands');
+    }
+};
